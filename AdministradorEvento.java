6c3012121bb12efd43b8f9e51065f83a50dd01e3
JPS4321 <sol22102@uvg.edu.gt>
2022-10-08 21:20:00 -0600

    Creacion de archivo main

---
+++ AdministradorEvento.java
@@ -0,0 +1,21 @@
+public class AdministradorEvento {
+    private String Nombre;
+    private String Documento_Identificacion;
+
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public String getDocumento_Identificacion() {
+        return Documento_Identificacion;
+    }
+    public void setDocumento_Identificacion(String documento_Identificacion) {
+        Documento_Identificacion = documento_Identificacion;
+    }
+    
+
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+//Juan Pablo Solis
+public class Main {
+    //Main: Permite que se ejecute el codigo adentro de las distintas clases
+    //Se debe de crear un objeto de la clase para que se ejecute.
+    public static void main (String [ ] args) {
+        //Un nuevo objeto tipo vista cuyo nombre es vista 1
+        vista vista1 = new vista();
+
+
+
+    }
+
+
+}
