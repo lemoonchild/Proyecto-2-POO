a955f9c0fe06707ca82e5cf151b7048ab2defbe4
Nahomy Castro <98111102+lemoonchild@users.noreply.github.com>
2022-11-07 17:42:12 -0600

    Cambios a la clase
    Se agregó descripción

--- Proyecto Final, entrega 2/Modelo/ListaDeUsuarios.java
+++ Proyecto Final, entrega 2/Modelo/ListaDeUsuarios.java
@@ -11,6 +11,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ListaDeUsuarios implements Serializable {
+    
     public ArrayList<Administrador_Evento> UsuariosAdministrativos = new ArrayList<Administrador_Evento>();
     public ArrayList<Voluntario> UsuariosVoluntarios = new ArrayList<Voluntario>();
 
