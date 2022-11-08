8ce71c4ac824041f505ac75246a98e82eb4ad100
Fabiola Contreras <98127191+Fabiola-cc@users.noreply.github.com>
2022-11-07 20:12:49 -0600

    adición de comentarios

--- Proyecto-Final_entrega3/Controlador/Serializar.java
+++ Proyecto-Final_entrega3/Controlador/Serializar.java
@@ -1,9 +1,9 @@
-package Controlador;
-/*
- * 
+/* 
  * Clase serializar
  * Establece funciones para guardar y recabar datos desde un archivo.
  */
+
+package Controlador;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,13 +19,14 @@ public class Serializar {
      * Los guarda en el formato necesario para que la puedan volver a leerse
      * después.
      * 
-     * @param <T>
-     * @param donde
-     * @param que
+     * @param <tipo> tipo variable, el método puede recibir datos de una variedad de
+     *               tipos
+     * @param donde  archivo en el que se registran los datos
+     * @param que    los datos a registrar
      * @throws IOException
      * @throws ClassNotFoundException
      */
-    public static <T> void escribir(File donde, T que) throws IOException, ClassNotFoundException {
+    public static <tipo> void escribir(File donde, tipo que) throws IOException, ClassNotFoundException {
         FileOutputStream escritor = new FileOutputStream(donde); // Ubica el archivo donde guardar los datos
         try (ObjectOutputStream objeto = new ObjectOutputStream(escritor)) { // Escribe los datos como stream en el
                                                                              // archivo.
@@ -37,9 +38,10 @@ public class Serializar {
      * Revisa el documento dado y vuelve a cargar los datos que fueron guardados
      * ahí.
      * 
-     * @param <tipo>
-     * @param Porleer
-     * @return
+     * @param <tipo>  tipo variable, el método puede recibir datos de una variedad
+     *                de tipos
+     * @param Porleer el documento del cual se sacan los datos
+     * @return todos los datos obtenidos del archivo leído
      * @throws IOException
      * @throws ClassNotFoundException
      */
